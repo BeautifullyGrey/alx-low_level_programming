ba99bfac72783de2bc7c48cdbf1c59f483898a01
BeautifullyGrey <fullybeautigrey@gmail.com>
2022-03-16 04:55:47 -0700

    fix

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,29 +1,15 @@
 #include "main.h"
-#include<stdio.h>
 
 /**
- * print_sign - prints the sign of a number
- *
- * @c: number to check
- *
- * Return: +1 if is positive, 0 if is 0 and -1 if is negative
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
  */
-
-int print_sign(int c)
+int _abs(int n)
 {
-	if (c > 0)
+	if (n >= 0)
 	{
-		_putchar('+');
-		return (1);
+		return (n);
 	}
-	else if (c == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
-
+	return (-n);
+}
